b8f611fb64833fad7d2b40fdc4c3be3e97b26e9a
deviswetha <122210461+deviswetha@users.noreply.github.com>
2025-11-13 23:26:55 +0530

    Add decision-making statements in Python

---
+++ decision_making_stmt.py
@@ -0,0 +1,38 @@
+'''dectision making statemwnts '''
+
+'''num = int(input("enter the number :"))
+
+if num>0:
+    print('postive number')
+    print('end of the  if  block')
+else :
+    print('negative')
+    print('end of the else block')'''
+
+'''num=int(input('enther the number:'))
+
+if num % 2==0:
+    print('even')
+else:
+    print('odd')'''
+'''
+n1 = int(input('enter the n1 :'))
+n2 = int (input('enter the n2:'))
+
+if n1<n2:
+   print("bigest is ",n2)
+else:
+   print('bigest is',n1)'''
+
+'''x=10
+
+if x:
+    print('yes')
+else:
+    print('no')'''
+
+'''if x=0:
+    print('yes')
+else:
+    print('no')'''
+    
